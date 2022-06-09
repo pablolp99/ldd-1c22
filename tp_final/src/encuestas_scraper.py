e740ddf0777e1913add117ac1226b241e0083f60
Pablo Lopes Perera <plopesperera99@gmail.com>
2022-06-09 00:17:15 -0300

    Mas cosas

--- tp_final/src/encuestas_scraper.py
+++ tp_final/src/encuestas_scraper.py
@@ -1,12 +1,14 @@
 import json
+import numpy as np
+import pandas as pd
 import re
-import time
 
 from PIL import Image
 import requests
 from io import BytesIO
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 PIXELS = {
     0: (13, 13),
@@ -23,6 +25,11 @@ PIXELS = {
     11: (332, 13)
 }
 
+with open('avg_colors.json', 'r') as f:
+    data = json.load(f)
+
+COLORS_DICT = data
+COLORS_DF = pd.DataFrame([COLORS_DICT]).T
 
 if __name__ == '__main__':
 
@@ -30,7 +37,7 @@ if __name__ == '__main__':
 
     options.add_argument("start-maximized")
     options.add_argument("enable-automation")
-    #options.add_argument("--headless")
+    # options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-infobars")
     options.add_argument("--disable-dev-shm-usage")
@@ -44,6 +51,21 @@ if __name__ == '__main__':
     with open("docentes.json", "r") as file:
         docentes = json.load(file)
 
+
+    doc_results = pd.DataFrame(
+        columns=[
+            'doc_name',
+            'term',
+            'year',
+            'student_count',
+            'course',
+            'course_id',
+            'course_url',
+            'doc_survey',
+        ]
+    )
+
+
     for k, v in docentes.items():
 
         doc_name = list(v.keys())[0]
@@ -51,13 +73,27 @@ if __name__ == '__main__':
         
         wd.get(survey_results)
         # Click to get only colors:
-        wd.find_elements_by_xpath("//span[@class='nbtype']//a")[1].click()
+        wd.find_elements(By.XPATH, "//span[@class='nbtype']//a")[1].click()
 
-        doc_data = wd.find_elements_by_xpath("//table[@class='inline']//tr")[2:]
+        doc_data = wd.find_elements(By.XPATH, "//table[@class='inline']//tr")[2:]
         # El par docente-materia comienza en los indices 0 y 1
-        # Luego salta de a 4 y 5, asi continua. 
+        # Luego salta de a 4 y 5, asi continua.
+
+        tmp_df = pd.DataFrame(
+            columns=[
+                'doc_name',
+                'term',
+                'year',
+                'student_count',
+                'course',
+                'course_id',
+                'course_url',
+                'doc_survey',
+            ]
+        )
 
         for i in range(0, len(doc_data), 4):
+
             # Encuesta docente: i
             # Materia: i+1
             survey = doc_data[i]
@@ -65,21 +101,36 @@ if __name__ == '__main__':
 
             m = re.search(r"((2c|1c|v)(\d{4}))\s(\d*)", survey.text)
             
-            semester = m.group(2)
+            term = m.group(2)
             year = m.group(3)
-            students = m.group(4)
+            student_count = m.group(4)
             course_name = course.text.strip()            
 
-            img_url = survey.find_element_by_xpath(".//img").get_attribute("src")
+            img_url = survey.find_element(By.XPATH, ".//img").get_attribute("src")
 
             response = requests.get(img_url)
             img = Image.open(BytesIO(response.content))
             pixels = img.load()
             
+            doc_survey = []
             for q, p in PIXELS.items():
-                breakpoint()
                 
-                print(img[p])
+                result = float(COLORS_DF.apply(
+                    lambda x: np.linalg.norm( np.array(x[0]) - np.array(pixels[p])), axis=1
+                ).idxmin())
+                doc_survey.append(result)
 
-        wd.close()
-        exit(0)
+            _tmp = pd.Series()
+            _tmp['doc_name'] = doc_name
+            _tmp['term'] = term
+            _tmp['year'] = year
+            _tmp['student_count'] = student_count
+            _tmp['course'] = course_name
+            _tmp['course_id'] = hash(f"{course_name}{term}{year}")
+            _tmp['course_url'] = course.find_element(By.XPATH, ".//a").get_attribute("href")
+            _tmp['doc_survey'] = doc_survey
+            tmp_df = tmp_df.append(_tmp, ignore_index=True)
+        
+        doc_results = pd.concat([doc_results, tmp_df])
+
+        doc_results.to_csv("doc_results.csv")
